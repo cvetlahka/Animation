91403b610a89f39037337edaaef84d2bbdf303c0
Safonova Svetlana <Safonova_s@icloud.com>
2020-11-14 15:18:50 +0300

    анимация

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -5,15 +5,41 @@
 //  Created by Светлана Сафонова on 14.11.2020.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var coreAnimationView: UIView!
+    
+    @IBOutlet var springAnimationVeiw: SpringView!
+    private var originCoordinate: CGFloat?
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        originCoordinate = coreAnimationView.frame.origin.x
     }
 
-
+    @IBAction func coreAnimationButtonPressed(_ sender: UIButton) {
+        sender.pulsate()
+        
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            options: [.autoreverse, .repeat ]) {
+            if self.coreAnimationView.frame.origin.x == self.originCoordinate{
+                self.coreAnimationView.frame.origin.x -= 40
+            }
+        }
+    }
+    
+    @IBAction func rubSpringAnimationButton(_ sender: SpringButton) {
+        springAnimationVeiw.animation = "fadeIn"
+        springAnimationVeiw.curve = "easeIn"
+        springAnimationVeiw.duration = 1
+        springAnimationVeiw.animate()
+        
+        sender.animation = "shake"
+        sender.animate()
+    }
 }
 
